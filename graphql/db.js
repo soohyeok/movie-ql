5a5aa19b1fa9f97b29775f00045d80202071416b
soohyeok <soohyeok0822@gmail.com>
2020-12-17 20:45:12 -0700

    wrapping REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,54 +1,17 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Avengers",
-        score: 9
-    },
-    {
-        id: 1,
-        name: "Up",
-        score: 7
-    },
-    {
-        id: 2,
-        name: "Star Wars",
-        score: 6
-    },
-    {
-        id: 3,
-        name: "The Godfather",
-        score: 9
-    },
-    {
-        id: 4,
-        name: "Spiderman",
-        score: 6
-    },
-    
-];
-export const getMovies = () => movies;
+import fetch from 'node-fetch';
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0];
-};
+const API_URL = "https://yts-proxy.now.sh/list_movies.json?";
 
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    }else{
-        return false;
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if(limit > 0){
+        REQUEST_URL += `limit=${limit}`;
     }
+    if(rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(json=> json.data.movies);
 };
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: (movies.length),
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-};
